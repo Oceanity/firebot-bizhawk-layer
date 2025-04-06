878811e3ce9f860a02fac9e6d3bda8d455e95c23
Izzy Deane <github@oceanity.net>
2025-04-05 23:32:47 -0700

    Initial setup

--- README.md
+++ README.md
@@ -1,18 +1,37 @@
-# Starter Firebot Custom Script in Typescript
+# Bizhawk Compatibility Layer (by Oceanity)
+
+This script was created to allow Bizhawk Lua scripts to send `POST` requests to Firebot. By default, sending a `POST` request from Bizhawk using it's `comm.httpPost` method sends a body like this with no way to adjust it:
+
+```json
+{
+  "payload": "{ \"foo\": \"bar\" }"
+}
+```
+
+This script adds a new endpoint to Firebot, `/integrations/oceanity/bizhawk` that takes `POST` requests with a payload like this (needs to be string encoded in Lua script):
+
+```json
+{
+  "url": "/api/v1/counters/my-counter-id-here", // whatever Firebot endpoint you want to pass through too
+  "data": {
+    // your request JSON
+    "value": 1
+  }
+}
+```
+
+and passes it on to the actual Firebot endpoint, returning the response as you would expect.
 
 ### Setup
-1. Create a new repo based off this template (Click "Use this Template" above) or simply fork it
-2. `npm install`
 
-### Building
-Dev:
-1. `npm run build:dev`
-- Automatically copies the compiled .js to Firebot's scripts folder.
+- In Firebot, go to Settings > Scripts
+  - Enable Custom Scripts if they are not currently enabled
+  - Click Manage Startup Scripts
+  - Click Add New Script
+  - Click the "scripts folder" link to open the Scripts Folder and place `oceanityBizhawkCompatibilityLayer.js` there
+  - Refresh the list of scripts and pick `oceanityBizhawkCompatibilityLayer.js` from the dropdown
 
-Release:
-1. `npm run build`
-- Copy .js from `/dist`
+### Updating
 
-### Note
-- Keep the script definition object (that contains the `run`, `getScriptManifest`, and `getDefaultParameters` funcs) in the `main.ts` file as it's important those function names don't get minimized.
-- Edit the `"scriptOutputName"` property in `package.json` to change the filename of the outputted script.
+- Overwrite existing `oceanityBizhawkCompatibilityLayer.js` with new version
+- Restart Firebot
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,68 @@
 import { Firebot } from "@crowbartools/firebot-custom-scripts-types";
+import { Request, Response } from "express";
 
-interface Params {
-  message: string;
-}
+interface Params {}
+
+type BizhawkRequest = {
+  url: string;
+  data: Record<any, any>;
+};
 
 const script: Firebot.CustomScript<Params> = {
   getScriptManifest: () => {
     return {
-      name: "Starter Custom Script",
-      description: "A starter custom script for build",
-      author: "SomeDev",
+      name: "Bizhawk Compatibility Layer (by Oceanity)",
+      description:
+        "Registers an endpoint that properly forwards Bizhawk POST requests",
+      author: "Oceanity",
       version: "1.0",
       firebotVersion: "5",
     };
   },
-  getDefaultParameters: () => {
-    return {
-      message: {
-        type: "string",
-        default: "Hello World!",
-        description: "Message",
-        secondaryDescription: "Enter a message here",
-        title: "Hello!",
-      },
-    };
-  },
+  getDefaultParameters: () => ({}),
   run: (runRequest) => {
-    const { logger } = runRequest.modules;
-    logger.info(runRequest.parameters.message);
+    const { logger, httpServer } = runRequest.modules;
+
+    httpServer.registerCustomRoute(
+      "oceanity",
+      "/bizhawk",
+      "POST",
+      async (req: Request, res: Response) => {
+        logger.info(JSON.stringify(req.body));
+
+        try {
+          let payload = JSON.parse(req.body.payload) as BizhawkRequest;
+
+          if (!payload.url) {
+            throw new Error("Missing required param `url`");
+          }
+
+          if (!payload.data) {
+            throw new Error("Missing required param `data`");
+          }
+
+          const response = await fetch(payload.url, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload.data),
+          });
+
+          if (response.status !== 200) {
+            throw new Error(
+              `Failed to forward request to ${payload.url}, status: ${response.status}`
+            );
+          }
+
+          res.status(response.status).send(await response.text());
+        } catch (err) {
+          logger.error("Error parsing payload", err);
+          res.status(400).send(err.message);
+          return;
+        }
+      }
+    );
   },
 };
 
